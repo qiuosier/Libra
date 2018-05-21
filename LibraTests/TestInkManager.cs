045a4596227090f138e53e044c170096033a6e20
Qiu <qiuosier@gmail.com>
2018-05-20 22:02:49 -0400

    Add Test Project.

---
+++ LibraTests/TestInkManager.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Windows.ApplicationModel;
+using Windows.Storage;
+using Windows.UI.Input.Inking;
+
+namespace LibraTests
+{
+    [TestClass]
+    public class TestInkManager
+    {
+        [TestMethod]
+        public async Task TestFileAccess()
+        {
+            StorageFolder assetsFolder = await Package.Current.InstalledLocation.GetFolderAsync("TestAssets");
+            StorageFile testFile = await assetsFolder.GetFileAsync("SinglePageTestFile.pdf");
+            Assert.IsNotNull(testFile);
+        }
+    }
+}
