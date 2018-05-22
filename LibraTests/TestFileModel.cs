2e1b12e374d9aa24d404ba18b155a7b38417443f
Qiu <qiuosier@gmail.com>
2018-05-21 21:25:20 -0400

    Add load file test.

--- LibraTests/TestFileModel.cs
+++ LibraTests/TestFileModel.cs
@@ -5,11 +5,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Windows.ApplicationModel;
 using Windows.Storage;
 using Windows.UI.Input.Inking;
+using Libra.Class;
 
 namespace LibraTests
 {
     [TestClass]
-    public class TestInkManager
+    public class TestFileModel
     {
         [TestMethod]
         public async Task TestFileAccess()
@@ -17,6 +18,14 @@ namespace LibraTests
             StorageFolder assetsFolder = await Package.Current.InstalledLocation.GetFolderAsync("TestAssets");
             StorageFile testFile = await assetsFolder.GetFileAsync("SinglePageTestFile.pdf");
             Assert.IsNotNull(testFile);
+        }
+
+        [TestMethod]
+        public async Task TestLoadFile()
+        {
+            StorageFile testFile = await Package.Current.InstalledLocation.GetFileAsync("TestAssets\\SinglePageTestFile.pdf");
+            StorageFolder dataFolder = await ApplicationData.Current.LocalFolder.CreateFolderAsync("data_folder");
+            PdfModel pdfModel = await PdfModel.LoadFromFile(testFile, dataFolder);
         }
     }
 }
